c295c062f5b56dde456f83936775ac2265cb4ef8
Suresh Munnangi <sureshmunnangi@gmail.com>
2016-10-06 20:46:16 -0400

    commit

--- src/app/app.supplier.pipe.ts
+++ src/app/app.supplier.pipe.ts
@@ -11,7 +11,8 @@ transform(supplierNames:any,searchTerm:any):any{
 if (searchTerm===undefined) return supplierNames;
 return supplierNames.filter(
     function(currentItem){
-        return currentItem.name.toLowerCase().includes(searchTerm.toLowerCase())
+        return currentItem.name.toLowerCase().includes(searchTerm.toLowerCase()) 
+        || currentItem.address.toLowerCase().includes(searchTerm.toLowerCase()) 
     }
 )
 }
